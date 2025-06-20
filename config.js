5dda083c4f535ed0e2b3dcd550f7f058f49ce62f
Piyush <piyushbhagwat5@gmail.com>
2025-06-20 11:06:10 +0530

    increades time

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const anuName = "Diamond";
 const piName = "Elixir";
 
-const AUTO_DELETE_TIME = 2 * 60; //in minutes
-const TOKEN_EXPIRY_TIME = 10; //min
+const AUTO_DELETE_TIME = 3 * 60; //in minutes /messages auto delete 
+const TOKEN_EXPIRY_TIME = 10; //min /login token
 
 export { anuName, piName, AUTO_DELETE_TIME, TOKEN_EXPIRY_TIME };
